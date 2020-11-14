7785088d0264fe3186030e94249883a002e4e17e
Juan Dorado <fdorado985@gmail.com>
2020-11-14 16:28:27 -0600

    Show the total amount of the cart and add the total logic on cart provider, connect the cart to the cart screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop_flutter/providers/cart.dart';
+import 'package:provider/provider.dart';
 
 class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Cart'),
@@ -14,7 +18,19 @@ class CartScreen extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Row(
-                children: [],
+                children: [
+                  Text(
+                    'Total',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Chip(
+                    label: Text('\$${cart.totalAmount.toString()}'),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  )
+                ],
               ),
             ),
           ),
