e25df8db8d9e02a90a9b740ffadbfc605bf92b33
Juan Dorado <fdorado985@gmail.com>
2020-11-14 16:53:35 -0600

    Create the ListView to show the items on cart screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -48,6 +48,13 @@ class CartScreen extends StatelessWidget {
               ),
             ),
           ),
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (ctx, i) {},
+            ),
+          ),
         ],
       ),
     );
