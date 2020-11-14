c55547ea35be9bc4450d2a1612111044572890d3
Juan Dorado <fdorado985@gmail.com>
2020-11-14 15:55:19 -0600

    Create the shopping cart data to notify cart changes

---
+++ lib/providers/cart.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    @required this.id,
+    @required this.title,
+    @required this.quantity,
+    @required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {...items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+        productId,
+        (oldItem) => CartItem(
+            id: oldItem.id,
+            title: oldItem.title,
+            quantity: oldItem.quantity + 1,
+            price: oldItem.price),
+      );
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+            id: DateTime.now().toString(),
+            price: price,
+            quantity: 1,
+            title: title),
+      );
+    }
+  }
+}
