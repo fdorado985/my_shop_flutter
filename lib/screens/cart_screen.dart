d191df559832f2f03f65a0f1db4dcf300c892b46
Juan Dorado <fdorado985@gmail.com>
2020-11-14 16:23:22 -0600

    Create the Cart screen with a basic empty design

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CartScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Cart'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.all(15),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
