406210645d3e5888af0125fd95e769824bae2366
Juan Dorado <fdorado985@gmail.com>
2020-11-14 23:16:00 -0600

    Add the methods and properties for the Orders notifier model

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -15,4 +15,23 @@ class OrderItem {
   });
 }
 
-class Orders with ChangeNotifier {}
+class Orders with ChangeNotifier {
+  List<OrderItem> _orders = [];
+
+  List<OrderItem> get orders {
+    return [..._orders];
+  }
+
+  void addOrder(List<CartItem> cartProducts, double total) {
+    _orders.insert(
+      0,
+      OrderItem(
+        id: DateTime.now().toString(),
+        amount: total,
+        products: cartProducts,
+        dateTime: DateTime.now(),
+      ),
+    );
+    notifyListeners();
+  }
+}
