f34c005d02fe1a6b2e0ecadb2458315eeab576e0
Juan Dorado <fdorado985@gmail.com>
2020-11-14 23:38:13 -0600

    Create the route for orders_screen and an app drawer

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,6 +4,8 @@ import 'package:my_shop_flutter/widgets/order_item.dart';
 import 'package:provider/provider.dart';
 
 class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
+
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<Orders>(context);
