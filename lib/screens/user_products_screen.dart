e6952d66498f4289f3879349557a33e09608c2e1
Juan Dorado <fdorado985@gmail.com>
2020-11-15 13:15:19 -0600

    Create the user products screen and content and a new widget for the user product items

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop_flutter/providers/products.dart';
+import 'package:my_shop_flutter/widgets/user_product_item.dart';
+import 'package:provider/provider.dart';
 
 class UserProductsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final productsData = Provider.of<Products>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: productsData.items.length,
+          itemBuilder: (_, i) => UserProductItem(
+            title: productsData.items[i].title,
+            imageUrl: productsData.items[i].imageUrl,
+          ),
+        ),
+      ),
+    );
   }
 }
