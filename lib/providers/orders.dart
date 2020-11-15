6a36f411076708c40ae4cebe599ebe26a2eef0b5
Juan Dorado <fdorado985@gmail.com>
2020-11-14 23:10:24 -0600

    Create the Order Item model to store each order

---
+++ lib/providers/orders.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+import 'package:my_shop_flutter/providers/cart.dart';
+
+class OrderItem {
+  final String id;
+  final double amount;
+  final List<CartItem> products;
+  final DateTime dateTime;
+
+  OrderItem({
+    @required this.id,
+    @required this.amount,
+    @required this.products,
+    @required this.dateTime,
+  });
+}
+
+class Orders with ChangeNotifier {}
