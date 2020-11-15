878d28f329a03a917a57c5d80380fc44059a261c
Juan Dorado <fdorado985@gmail.com>
2020-11-15 13:25:56 -0600

    Add a Divider between user product items and the AppDrawer

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_shop_flutter/providers/products.dart';
+import 'package:my_shop_flutter/widgets/app_drawer.dart';
 import 'package:my_shop_flutter/widgets/user_product_item.dart';
 import 'package:provider/provider.dart';
 
@@ -20,13 +21,19 @@ class UserProductsScreen extends StatelessWidget {
           )
         ],
       ),
+      drawer: AppDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(8),
         child: ListView.builder(
           itemCount: productsData.items.length,
-          itemBuilder: (_, i) => UserProductItem(
-            title: productsData.items[i].title,
-            imageUrl: productsData.items[i].imageUrl,
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                title: productsData.items[i].title,
+                imageUrl: productsData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
           ),
         ),
       ),
