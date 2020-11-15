01508f1c1b079864f27859c9b8576f8f4245f5bd
Juan Dorado <fdorado985@gmail.com>
2020-11-14 23:22:14 -0600

    Use the Orders provider to add our current cart to orders and clear our cart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_shop_flutter/providers/cart.dart' show Cart;
+import 'package:my_shop_flutter/providers/orders.dart';
 import 'package:my_shop_flutter/widgets/cart_item.dart';
 import 'package:provider/provider.dart';
 
@@ -41,7 +42,13 @@ class CartScreen extends StatelessWidget {
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                        cart.items.values.toList(),
+                        cart.totalAmount,
+                      );
+                      cart.clear();
+                    },
                     child: Text('ORDER NOW!'),
                     textColor: Theme.of(context).primaryColor,
                   )
