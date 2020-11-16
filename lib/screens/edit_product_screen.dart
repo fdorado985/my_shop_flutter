a45424899307aecc655bcfbbd31330d0c7b327db
Juan Dorado <fdorado985@gmail.com>
2020-11-15 22:51:07 -0600

    Add the product to provider and pop the edit product screen

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_shop_flutter/providers/product.dart';
+import 'package:my_shop_flutter/providers/products.dart';
+import 'package:provider/provider.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
@@ -235,5 +237,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
       return;
     }
     _form.currentState.save();
+    Provider.of<Products>(context, listen: false).addProduct(_editedProduct);
+    Navigator.of(context).pop();
   }
 }
